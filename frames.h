a10814846226279c11a5870d5716ef05d6d1837e
Matthew M. Burke <matthew@bluedino.net>
2017-12-12 22:18:27 -0500

    initial commit (5/6)

---
+++ frames.h
@@ -0,0 +1,18 @@
+
+#pragma once
+
+#include <SSD1306Wire.h>
+#include <OLEDDisplayUi.h>
+
+void drawDateTime(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y);
+void drawCurrency0(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y);
+void drawCurrency1(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y);
+void drawCurrency2(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y);
+void drawCurrency3(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y);
+void drawCurrency(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y, String symbol, String price);
+void drawHeaderOverlay(OLEDDisplay *display, OLEDDisplayUiState* state);
+void drawProgress(OLEDDisplay *display, int percentage, String label);
+void updateData(OLEDDisplay *display);
+void setReadyForUpdate();
+
+
